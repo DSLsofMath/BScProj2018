2ee417b12665a3344a13343de2890d3ce512ea7c
Oskar Lundström <osklunds@student.chalmers.se>
2018-01-31 13:25:40 +0100

    Experiment med DSL för kinematik

---
+++ Oskar_test/K2.hs
@@ -0,0 +1,52 @@
+
+module K2
+(
+)
+where
+
+-- s, v, t
+data Dis
+data Vel
+data Tim
+
+-- ds, dt (infinitesimala)         
+data DisI
+data TimI
+
+data Equ lhs rhs
+
+axi = undefined -- axiom
+
+
+
+integDis :: DisI -> Dis
+integTim :: TimI -> Tim
+
+
+
+-- likhet är som ekvivalens
+-- 
+
+-- s = v * t
+velMtimEdis :: Vel -> Tim -> Dis
+velMtimEdis = axi
+
+
+-- v = s / t
+disDvelEtim :: Dis -> Vel -> Tim
+disDvelEtim s v = 
+
+
+
+
+
+
+
+u = undefined
+velMtim = u
+disDvel = u
+disDtim = u
+
+integDis = u
+integTim = u
+momentousV = u

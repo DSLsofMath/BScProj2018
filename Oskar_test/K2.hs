6d85a46722d827258e91a5f9fc3a7e950cc12dca
Oskar Lundström <osklunds@student.chalmers.se>
2018-01-31 14:54:20 +0100

    Bevis av fler satser

--- Oskar_test/K2.hs
+++ Oskar_test/K2.hs
@@ -30,15 +30,22 @@ mulDivElim2 :: Mul (Div b a) a -> b
 mulDivElim2 bDaMa = let aMbDa = mulCom bDaMa
                     in  mulDivElim1 aMbDa
 
+mulDivElim3 :: Div (Mul a b) b -> a
+mulDivElim3 = undefined
+
+mulDivElim4 :: Div (Mul a b) a -> b
+mulDivElim4 = undefined
+
+
 
 data Equ lhs rhs = Equ lhs rhs
 
 mult :: (Equ lhs rhs) -> f -> Equ (Mul lhs f) (Mul rhs f)
 mult = axiom
 
+divv :: (Equ lhs rhs) -> d -> Equ (Div lhs d) (Div rhs d)
+divv = axiom
 
-op :: (Equ a a) -> (a -> t) -> (Equ t t)
-op = axiom
 
 
 eqIntro :: a -> b -> Equ a b
@@ -48,12 +55,22 @@ eqElim :: Equ a b -> (a, b)
 eqElim = axiom
 
 
+vEsDt :: Equ Vel (Div Dis Tim)
+vEsDt = axiom
+
 sEvMt :: Equ Dis (Mul Vel Tim)
 sEvMt = let vMtE'sDt'Mt = mult vEsDt Tim
             (vMt, sDtMt) = eqElim vMtE'sDt'Mt
             s = mulDivElim2 sDtMt
         in eqIntro s vMt
 
-vEsDt :: Equ Vel (Div Dis Tim)
-vEsDt = axiom
+-- TODO: Lösa ovanstående så att man inte kan fuska
+-- genom att byta vMt mot vad som helst
+
+tEsDv :: Equ Tim (Div Dis Vel)
+tEsDv = let sDvE'vMt'Dv = divv sEvMt Vel
+            (sDv, vMtDv) = eqElim sDvE'vMt'Dv
+            t = mulDivElim4 vMtDv
+        in eqIntro t sDv
+            
 

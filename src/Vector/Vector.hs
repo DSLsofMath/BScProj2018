368bfb2c052e969343b3c47be5aae172a576a661
Erik Sjöström <erikdsjostrom@gmail.com>
2018-02-06 21:50:56 +0100

    Vectors are monoids

--- src/Vector/Vector.hs
+++ src/Vector/Vector.hs
@@ -1,110 +1,103 @@
 module Vector where
 
--- Might just complicate things more
-data Coordinate a = Coord a a
+type Scalar = Double
 
--- | Är vektorer definierade från origo, eller fritt i rummet?
+class Vector vec where
+  vmap      :: (Scalar -> Scalar) -> vec -> vec
+  vzipWith  :: (Scalar -> Scalar  -> Scalar) -> vec -> vec -> vec
+  vfold     :: (Scalar -> Scalar  -> Scalar) -> vec -> Scalar
 
--- | Vektorer som vet sin "vinkel" och "storlek"?
+vdot :: Vector vec => vec -> vec -> Scalar
+vdot v1 v2 = vfold (*) $ vzipWith (+) v1 v2
 
--- | Could also only have V3 and leave the the third
--- | argument blank for V2
+vmag :: Vector vec => vec -> Scalar
+vmag v = sqrt . vfold (+) $ vmap (**2) v
 
--- | Vectors are only used in reference to the the object
+data Vector3 = V3 Scalar Scalar Scalar
+data Vector2 = V2 Scalar Scalar
 
 
--- Stupid name
-data Vector numb =
-      Scalar numb
-    | V2 (numb, numb)
-    | V3 (numb, numb, numb)
-    -- General vector type, with unlimited dimensions. And for each dimension
-    | V [numb]
-    deriving Show
+vLift :: Vector2 -> Vector3
+vLift (V2 x y) = V3 x y 0
 
-data VectorOP numb =
-    Magnitude (Vector numb)
-  | Addition  (Vector numb) (Vector numb)
-  | ScalarMult numb (Vector numb)
-  | DotProd   (Vector numb) (Vector numb)
-  | CrossProd (Vector numb) (Vector numb)
+v1 = V2 1 2
+v2 = V2 3 4
 
--- Floating type constraints because of my use of sqrt below.
+zeroVector :: (Vector a, Num a) => a
+zeroVector = 0
 
-addition :: (Floating t) => Vector t -> Vector t -> VectorOP t
-addition = Addition
+crossProd :: Vector3 -> Vector3 -> Vector3
+crossProd (V3 x y z) (V3 x' y' z') = V3 (y*z' - z*y') -- X
+                                        (z*x' - x*z') -- Y
+                                        (x*y' - y*x') -- Z
 
-scalarMult :: (Floating t) => t -> Vector t -> VectorOP t
-scalarMult = ScalarMult
-
-dotProd :: (Floating t) => Vector t -> Vector t -> VectorOP t
-dotProd = DotProd
-
-magnitude :: (Floating t) => Vector t -> VectorOP t
-magnitude = Magnitude
-
-instance Vector Num where
-  (Scalar i1) (+) (Scalar i2) = Scalar (i1 + i2)
-
--- Är monader OK?
--- Matriser?
-
--- Tallinje för att visa längd på vektorerna
-
+-- | TODO
 -- | ~Laws~
 -- | Langrange's formula: a x (b x c) = b(a * c) - c(a * b)
 -- | Cross product is anticommutative
 -- | Jacobi identity: a x (b x c) + b x (c x a) + c x (a x b) = 0
 
--- Jag saknar agda...
+-- | Unit vectors
+-- | Cross product
+-- | Angles
 
--- Check dimensions?
+-- | DONE
+-- | Associativity of addition follows from Monoid instance!
 
-lift :: (Floating t) => Vector t -> Vector t
-lift (V2 (x, y)) = V3 (x, y, 0)
+instance Monoid Vector2 where
+  mempty  = zeroVector
+  mappend = (+)
+  mconcat = foldr mappend mempty
 
-xUnit :: (Floating t) => Vector t
-xUnit = V2 (1, 0)
+instance Monoid Vector3 where
+  mempty  = zeroVector
+  mappend = (+)
+  mconcat = foldr mappend mempty
 
-eval :: (Floating t) => VectorOP t -> IO (Vector t)
-eval (Magnitude (V3 (x, y, z))) =
-  return $ Scalar $ sqrt $ x** 2 + y ** 2 + z ** 2
-eval (Addition (V3 (x1, y1, z1)) (V3 (x2, y2, z2))) =
-  return $ V3 (x1 + x2, y1 + y2, z1 + z2)
-eval (ScalarMult s (V3 (x, y, z))) =
-  return $ V3 (x * s, y * s, z * s)
-eval (DotProd (V3 (x1, y1, z1)) (V3 (x2, y2, z2))) =
-  return $ Scalar $ (x1 + x2) * (y1 + y2) * (z1 + z2)
-eval (CrossProd (V3 (x1, y1, z1)) (V3 (x2, y2, z2))) =
-  return $ V3 (y1 * z2 - z1 * y2,
-               z1 * x2 - x1 * z2,
-               x1 * y2 - y1 * x2)
+instance Vector Vector3 where
+  vmap     f (V3 x y z)               = V3 (f x) (f y) (f z)
+  vzipWith f (V3 x y z) (V3 x' y' z') = V3 (f x x') (f y y') (f z z')
+  vfold    f (V3 x y z)               = f z $ f x y
 
-{-
-eval (Magnitude (V coords)) =
-  return $ Scalar $ sqrt . sum $ map (**2) coords
+instance Vector Vector2 where
+  vmap     f (V2 x y)            = V2 (f x) (f y)
+  vzipWith f (V2 x y) (V2 x' y') = V2 (f x x') (f y y')
+  vfold    f (V2 x y)            = f x y
 
--- Can add general vectors of different dimensions
-eval (Addition (V []) (V (y:ys)))  = return $ V ys
-eval (Addition (V (x:xs)) (V []))  = return $ V xs
-eval (Addition (V (x:xs)) (V (y:ys)))  = do
-  (V test) <- eval $ Addition (V xs) (V ys)
-  return $ V $ (x + y) : test
+instance Num Vector2 where
+  (+)           = vzipWith (+)
+  (*)           = undefined -- Vec -> Scalar
+  abs           = undefined -- Vec -> Scalar
+  negate        = vmap (*(-1))
+  -- | Signum can be though of as the direction of a vector
+  signum        = vmap signum
+  fromInteger i = V2 (fromInteger i) 0
 
-eval (ScalarMult s (V coords)) = return $ V [(x * s, y) | (x,y) <- coords]
+instance Num Vector3 where
+  (+)           = vzipWith (+)
+  (*)           = undefined -- Vec -> Scalar
+  abs           = undefined -- Vec -> Scalar
+  negate        = vmap (*(-1))
+  -- | Signum can be though of as the direction of a vector
+  signum        = vmap signum
+  fromInteger i = V3 (fromInteger i) 0 0
 
-eval (DotProd (V c1) (V c2)) =
-  let l1 = map (\(f,s) -> (s -f)) c1
-      l2 = map (\(f,s) -> (s -f)) c2
-   in return $ Scalar $ sum $ zipWith (+) l1 l2
+instance Show Vector2 where
+  show (V2 x y) = "(" ++ show x ++ " x, " ++ show y ++ " y)"
 
-eval (CrossProd v1@(V c1) v2@(V c2)) = case length c1 of
-                                         3 ->
-                                           do a <- eval $ magnitude v1
-                                              b <- eval $ magnitude v2
-                                              --return a * b * sin()
-                                              return $ V []
-                                         _ -> error "Wrong dimensions"
+instance Show Vector3 where
+  show (V3 x y z) = "(" ++ show x ++ " x, "
+                        ++ show y ++ " y, "
+                        ++ show z ++ " z)"
 
--}
-eval _                       = error "Not defined"
+instance Ord Vector2 where
+  compare v1 v2 = compare (vmag v1) (vmag v2)
+
+instance Ord Vector3 where
+  compare v1 v2 = compare (vmag v1) (vmag v2)
+
+instance Eq Vector2 where
+  (V2 x y) == (V2 x' y') = x == x' && y == y'
+
+instance Eq Vector3 where
+  (V3 x y z) == (V3 x' y' z') = x == x' && y == y' && z == z'

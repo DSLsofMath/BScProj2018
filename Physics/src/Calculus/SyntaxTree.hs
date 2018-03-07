468af386d78fe22264be097670f86c9f4b2c7ff6
erikdsjostrom <erikdsjostrom@gmail.com>
2018-03-07 13:31:52 +0100

    Added more proofs and funcs

--- Physics/src/Calculus/SyntaxTree.hs
+++ Physics/src/Calculus/SyntaxTree.hs
@@ -4,38 +4,27 @@ import Calculus.Calculus
 import Data.Tree as T
 import Data.Tree.Pretty as P
 
-eT = (D (Func "sin")) :+ Func "cos" :$ (Const 2 :+ Const 3) :* ((Const 3 :+ Const 2) :* (Delta (Const 1 :+ Const 2))) :/ (Const 5 :- (Const 4 :+ Const 8)) :+ (Lambda "x" (Const 2))
-e4 = (Const 1 :+ Const 2) :* (Const 3 :+ Const 4)
-e5 = (Const 1 :+ Const 2) :* (Const 4 :+ Const 3)
-
-syntacticProofOfAssocForMultiplication :: IO Bool
-syntacticProofOfAssocForMultiplication = prettyEqual e1 e2
-  where
-    e1 = (Const 3 :* Const 2) :* Const 5
-    e2 = Const 3 :* (Const 2 :* Const 5)
-
-syntacticProofOfComForAddition :: IO Bool
-syntacticProofOfComForAddition = prettyEqual e1 e2
-  where
-    e1 = (Const 3 :+ Const 2)
-    e2 = (Const 2 :+ Const 3)
-
-syntacticProofOfAssocForAddition :: IO Bool
-syntacticProofOfAssocForAddition = prettyEqual e1 e2
-  where
-    e1 = (Const 3 :+ Const 2) :+ Const 7
-    e2 = Const 7 :+ (Const 2 :+ Const 3)
-
-syntacticProofOfIdentityForAddition :: Expr -> IO Bool
-syntacticProofOfIdentityForAddition e = putStrLn "[*] Checking right identity" >> 
-  prettyEqual e (0 :+ e) >>
-    putStrLn "[*] Checking left identity" >>
-      prettyEqual e (e :+ 0) 
 
 main :: IO Bool
 main = prettyEqual e4 e5
 
+-- | Pretty prints expressions as trees
+printExpr :: Expr -> IO ()
+printExpr = putStrLn . drawVerticalTree . makeTree
+
+-- | Pretty prints the steps taken when canonifying an expression
+prettyCan :: Expr -> IO ()
+prettyCan e =  do
+  let t  = makeTree e
+      e' = canonify e
+      t' = makeTree e'
+    in if t == t' then (putStrLn $ drawVerticalTree t)
+                  else do
+                      (putStrLn $ drawVerticalTree t)
+                      prettyCan e'
+
 -- Possible generalization, make it work on lists of Expr
+-- | Pretty prints syntactic checking of equality
 prettyEqual :: Expr -> Expr -> IO Bool
 prettyEqual e1 e2 = case (e1 == e2) of
   True -> do 
@@ -50,29 +39,20 @@ prettyEqual e1 e2 = case (e1 == e2) of
       in if (c1 == e1 && c2 == e2) then putStrLn "Can't simplify no more" >> return False
                                    else prettyEqual c1 c2
 
-prettyCan :: Expr -> IO ()
-prettyCan e =  do
-  let t  = makeTree e
-      e' = canonify e
-      t' = makeTree e'
-    in if t == t' then (putStrLn $ drawVerticalTree t)
-                  else do
-                      (putStrLn $ drawVerticalTree t)
-                      prettyCan e'
-
+-- Parse an expression as a Tree of Strings
 makeTree :: Expr -> T.Tree String
-makeTree (e1 :+ e2) = T.Node ("+") [(makeTree e1), (makeTree e2)]
-makeTree (e1 :- e2) = T.Node ("-") [(makeTree e1), (makeTree e2)]
-makeTree (e1 :* e2) = T.Node ("*") [(makeTree e1), (makeTree e2)]
-makeTree (e1 :/ e2) = T.Node ("Div") [(makeTree e1), (makeTree e2)]
-makeTree (e1 :. e2) = T.Node ("∘") [(makeTree e1), (makeTree e2)]
-makeTree (Var v)    = T.Node v []
+makeTree (e1 :+ e2)   = T.Node ("+") [(makeTree e1), (makeTree e2)]
+makeTree (e1 :- e2)   = T.Node ("-") [(makeTree e1), (makeTree e2)]
+makeTree (e1 :* e2)   = T.Node ("*") [(makeTree e1), (makeTree e2)]
+makeTree (e1 :/ e2)   = T.Node ("Div") [(makeTree e1), (makeTree e2)]
+makeTree (e1 :. e2)   = T.Node ("o") [(makeTree e1), (makeTree e2)]
+makeTree (Var v)      = T.Node v []
 makeTree (Lambda s e) = T.Node ("λ " ++ s) [makeTree e]
-makeTree (Func s)   = T.Node s []
-makeTree (Delta e)  = T.Node "Δ" [makeTree e]
-makeTree (D e)      = T.Node "D" [makeTree e]
-makeTree (e1 :$ e2) = T.Node ("$") [(makeTree e1), (makeTree e2)]
-makeTree e          = T.Node (show e) []
+makeTree (Func s)     = T.Node s []
+makeTree (Delta e)    = T.Node "Δ" [makeTree e]
+makeTree (D e)        = T.Node "D" [makeTree e]
+makeTree (e1 :$ e2)   = T.Node ("$") [(makeTree e1), (makeTree e2)]
+makeTree e            = T.Node (show e) []
 
 
 equals :: Expr -> Expr -> Bool
@@ -138,3 +118,37 @@ canonify (D e) = derive e
 -- | Catch all
 canonify (Const x) = Const x
 canonify e = error $ show e
+
+
+-- | "Proofs"
+syntacticProofOfComForMultiplication :: Expr -> Expr -> IO Bool
+syntacticProofOfComForMultiplication e1 e2 = prettyEqual (e1 :* e2) (e2 :* e1)
+
+syntacticProofOfAssocForMultiplication :: Expr -> Expr -> Expr -> IO Bool
+syntacticProofOfAssocForMultiplication e1 e2 e3 = prettyEqual (e1 :* (e2 :* e3))
+                                                              ((e1 :* e2) :* e3)
+
+syntacticProofOfDistForMultiplication :: Expr -> Expr -> Expr -> IO Bool
+syntacticProofOfDistForMultiplication e1 e2 e3 = prettyEqual (e1 :* (e2 :+ e3))
+                                                              ((e1 :* e2) :+ (e1 :* e3))
+
+syntacticProofOfComForAddition :: Expr -> Expr -> IO Bool
+syntacticProofOfComForAddition e1 e2 = prettyEqual (e1 :+ e2) (e2 :+ e1)
+
+syntacticProofOfAssocForAddition :: Expr -> Expr -> Expr -> IO Bool
+syntacticProofOfAssocForAddition e1 e2 e3 = prettyEqual (e1 :+ (e2 :+ e3))
+                                                        ((e1 :+ e2) :+ e3)
+
+syntacticProofOfIdentityForAddition :: Expr -> IO Bool
+syntacticProofOfIdentityForAddition e = putStrLn "[*] Checking right identity" >> 
+  prettyEqual e (0 :+ e) >>
+    putStrLn "[*] Checking left identity" >>
+      prettyEqual e (e :+ 0) 
+
+-- | Dummy expressions
+eT = (D (Func "sin")) :+ Func "cos" :$ (Const 2 :+ Const 3) :* ((Const 3 :+ Const 2) :* (Delta (Const 1 :+ Const 2))) :/ (Const 5 :- (Const 4 :+ Const 8)) :+ (Lambda "x" (Const 2))
+e1 = Const 1
+e2 = Const 2
+e3 = Const 3
+e4 = (Const 1 :+ Const 2) :* (Const 3 :+ Const 4)
+e5 = (Const 1 :+ Const 2) :* (Const 4 :+ Const 3)

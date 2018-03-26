9b2ea9dbd059af9524d50ff37806b8509082b8c1
Erik Sjöström <erikdsjostrom@gmail.com>
2018-03-26 21:15:33 +0200

    Switching to master to fix stack problems

---
+++ Physics/src/NewtonianMechanics/SingleParticle.hs
@@ -0,0 +1,11 @@
+module SingleParticle where
+
+import           Vector.Vector
+
+-- Laws
+-- 1: A body remains at rest or in uniform motion unless acted upon by a force.
+-- 2: A body acted upon by a force moves in such a manner that the time rate of
+-- change of the momentum equals the force.
+-- 3: If two bodies exert forces on each other, these forces are equal un
+-- magnitude and opposite in direction.
+
